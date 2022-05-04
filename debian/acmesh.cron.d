3b5a91fc6674f2bb85ab4907f245c4146cdf7aa9
Lukhas <lbonnet@bearstech.com>
2022-05-04 12:01:39 +0000

    Update cron.d to send errors to syslog as well.

--- debian/acmesh.cron.d
+++ debian/acmesh.cron.d
@@ -2,7 +2,7 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # m  h dom mon dow   user command
-15 10  *   *   *     root  /usr/bin/acme.sh --quiet --cron --home /etc/acme
+15 10  *   *   *     root  /usr/bin/acme.sh --quiet --cron --home /etc/acme 2>&1 | tee /dev/stderr | logger
 
 # use the one below for dev/preprod servers where apache can be turned off
 # 15 10  *   *   *     root  /usr/bin/acme-wrapper
